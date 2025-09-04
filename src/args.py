9577095f3f2f78870fa8c84066c64202c49bdf24
StarStreammm <xxh_private@outlook.com>
2025-09-04 22:43:51 +0800

    bug fix

--- src/args.py
+++ src/args.py
@@ -40,7 +40,7 @@ def setup_args() -> argparse.ArgumentParser:
 
     # Version
     version = parser.add_argument_group("Version")
-    version.add_argument("-v", "--version", action='version', version='V1.2.0', help="Show program's version")
+    version.add_argument("-v", "--version", action='version', version='V1.2.1', help="Show program's version")
 
     # Get args && Args in file
     args = parser.parse_args()
